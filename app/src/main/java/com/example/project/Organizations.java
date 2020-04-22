4c83cc64628b9e577b50b51a3f578c889a35ac16
GandhaliShinde <gandhalishinde27@gmail.com>
2020-04-22 17:36:30 +0530

    22/04/2020 17:36PM

--- app/src/main/java/com/example/project/Organizations.java
+++ app/src/main/java/com/example/project/Organizations.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.net.Uri;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,31 +16,61 @@ public class Organizations extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.organizations);
-    }
 
-    public void site1(View view)
-    {
-        Intent i1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ayush.gov.in/covid-19-ayush-volunteers-0"));
+        TextView site1=findViewById(R.id.textView26);  //RAM CORPS
+        TextView site2=findViewById(R.id.textView32);  //UN
+        TextView site3=findViewById(R.id.textView28);  //iVOLUNTEER
+        TextView site4=findViewById(R.id.textView31);  //WAR AGAINST COVID
+
+
+
+    site1.setOnClickListener(v -> {
+        Intent i1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://volunteer.ramusa.org/"));
         startActivity(i1);
+    });
+
+        site2.setOnClickListener(v -> {
+            Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://self4society.mygov.in/"));
+            startActivity(i2);
+        });
+
+        site3.setOnClickListener(v -> {
+            Intent i3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.mygov.in/task/join-war-against-covid-19-register-volunteer/"));
+            startActivity(i3);
+        });
+
+        site4.setOnClickListener(v -> {
+            Intent i4 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.unv.org/Volunteers-for-Novel-Coronavirus-COVID-19-pandemic-response/"));
+            startActivity(i4);
+        });
 
     }
+
+
+
+
+
+
+
+
+
+
+    /////////////////
+
 
     public void site2(View view)
     {
 
-        Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://self4society.mygov.in/"));
-        startActivity(i2);
+
     }
     public void site3(View view)
     {
-        Intent i3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.mygov.in/task/join-war-against-covid-19-register-volunteer/"));
-        startActivity(i3);
+
     }
 
     public void site4(View view)
     {
-        Intent i4 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.unv.org/Volunteers-for-Novel-Coronavirus-COVID-19-pandemic-response/"));
-        startActivity(i4);
+
 
     }
 
