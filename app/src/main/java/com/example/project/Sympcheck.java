16602ea28b1e58014530d48dbaf6e2f520d30d45
imnik11 <nitesh4y2001@gmail.com>
2020-05-02 12:13:34 +0530

    sympt check corrected still miles to go...

--- app/src/main/java/com/example/project/Sympcheck.java
+++ app/src/main/java/com/example/project/Sympcheck.java
@@ -35,7 +35,7 @@ public class Sympcheck extends Activity {
                 return;
             }
             total = total + 3;
-            status(total);
+//            status(total);
             System.out.println( total );
             f.setBackgroundColor(getResources().getColor(R.color.change));
             none.setBackgroundColor(getResources().getColor(R.color.colorAccent));
@@ -52,7 +52,7 @@ public class Sympcheck extends Activity {
                 return;
             }
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println( total );
             c.setBackgroundColor(getResources().getColor(R.color.change));
             none.setBackgroundColor(getResources().getColor(R.color.colorAccent));
@@ -71,7 +71,7 @@ public class Sympcheck extends Activity {
                 return;
             }
             total = total + 8;
-            status(total);
+//            status(total);
             System.out.println( total );
             b.setBackgroundColor(getResources().getColor(R.color.change));
             none.setBackgroundColor(getResources().getColor(R.color.colorAccent));
@@ -90,7 +90,7 @@ public class Sympcheck extends Activity {
                 return;
             }
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println( total );
             s.setBackgroundColor(getResources().getColor(R.color.change));
             none.setBackgroundColor(getResources().getColor(R.color.colorAccent));
@@ -109,7 +109,7 @@ public class Sympcheck extends Activity {
             }
 
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println( total );
             d.setBackgroundColor(getResources().getColor(R.color.change));
             none.setBackgroundColor(getResources().getColor(R.color.colorAccent));
@@ -129,7 +129,7 @@ public class Sympcheck extends Activity {
             }
 
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println( total );
             h.setBackgroundColor(getResources().getColor(R.color.change));
             none.setBackgroundColor(getResources().getColor(R.color.colorAccent));
@@ -141,7 +141,7 @@ public class Sympcheck extends Activity {
         none.setOnClickListener(view -> {
 
             total = 1;
-            status(total);
+//            status(total);
 
             none.setBackgroundColor(getResources().getColor(R.color.change));
 
@@ -162,16 +162,16 @@ public class Sympcheck extends Activity {
         final TextView r = findViewById( R.id.textView20);
 
         r.setOnClickListener(view -> {
-            ImageView ques= findViewById(R.id.question);
-            ques.setVisibility(View.VISIBLE);
-            ImageView safe= findViewById(R.id.safe);
-            safe.setVisibility(View.INVISIBLE);
-            ImageView onverge= findViewById(R.id.onverge);
-            onverge.setVisibility(View.INVISIBLE);
-            ImageView notsafe= findViewById(R.id.notsafe);
-            notsafe.setVisibility(View.INVISIBLE);
-            ImageView danger= findViewById(R.id.danger);
-            danger.setVisibility(View.INVISIBLE);
+//            ImageView ques= findViewById(R.id.question);
+//            ques.setVisibility(View.VISIBLE);
+//            ImageView safe= findViewById(R.id.safe);
+//            safe.setVisibility(View.INVISIBLE);
+//            ImageView onverge= findViewById(R.id.onverge);
+//            onverge.setVisibility(View.INVISIBLE);
+//            ImageView notsafe= findViewById(R.id.notsafe);
+//            notsafe.setVisibility(View.INVISIBLE);
+//            ImageView danger= findViewById(R.id.danger);
+//            danger.setVisibility(View.INVISIBLE);
             f.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             b.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             s.setBackgroundColor(getResources().getColor(R.color.colorAccent));
@@ -206,56 +206,56 @@ public class Sympcheck extends Activity {
             }
         });
     }
-        private void status(int tot){
-            ImageView ques = findViewById(R.id.question);
-            ImageView safe= findViewById(R.id.safe);
-            ImageView onverge= findViewById(R.id.onverge);
-            ImageView notsafe= findViewById(R.id.notsafe);
-            ImageView danger= findViewById(R.id.danger);
-            if(tot==1){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-
-            else if(tot>1 && tot < 4){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.VISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-            else if((tot >= 4) && (tot < 9)){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.VISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-            else if((tot >= 9) && (tot < 14)){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.VISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-            else if( tot >= 14 ){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.VISIBLE);
-            } else {
-                //In case of zero or negative total
-                ques.setVisibility(View.VISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-        }
+//        private void status(int tot){
+//            ImageView ques = findViewById(R.id.question);
+//            ImageView safe= findViewById(R.id.safe);
+//            ImageView onverge= findViewById(R.id.onverge);
+//            ImageView notsafe= findViewById(R.id.notsafe);
+//            ImageView danger= findViewById(R.id.danger);
+//            if(tot==1){
+////                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//
+//            else if(tot>1 && tot < 4){
+////                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.VISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//            else if((tot >= 4) && (tot < 9)){
+////                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.VISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//            else if((tot >= 9) && (tot < 14)){
+////                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.VISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//            else if( tot >= 14 ){
+////                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.VISIBLE);
+//            } else {
+////                In case of zero or negative total
+////                ques.setVisibility(View.VISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//        }
         private void resetSymptoms(){
             for(int i =0 ;i<6; ++i){
                 symptoms[i] = "";
