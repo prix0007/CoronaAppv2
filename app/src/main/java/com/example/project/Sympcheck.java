6b9f61b1aedece3131b230c720cb4e06e65ac3c1
imnik11 <nitesh4y2001@gmail.com>
2020-05-11 13:23:22 +0530

    Image view remvoed working on android 6 sympt next done with android 6 successfully working

--- app/src/main/java/com/example/project/Sympcheck.java
+++ app/src/main/java/com/example/project/Sympcheck.java
@@ -37,16 +37,16 @@ public class Sympcheck extends Activity {
         final TextView r = findViewById( R.id.textView20);
 
         r.setOnClickListener(view -> {
-            ImageView ques= findViewById(R.id.question);
-            ques.setVisibility(View.VISIBLE);
-            ImageView safe= findViewById(R.id.safe);
-            safe.setVisibility(View.INVISIBLE);
-            ImageView onverge= findViewById(R.id.onverge);
-            onverge.setVisibility(View.INVISIBLE);
-            ImageView notsafe= findViewById(R.id.notsafe);
-            notsafe.setVisibility(View.INVISIBLE);
-            ImageView danger= findViewById(R.id.danger);
-            danger.setVisibility(View.INVISIBLE);
+//            ImageView ques= findViewById(R.id.question);
+//            ques.setVisibility(View.VISIBLE);
+//            ImageView safe= findViewById(R.id.safe);
+//            safe.setVisibility(View.INVISIBLE);
+//            ImageView onverge= findViewById(R.id.onverge);
+//            onverge.setVisibility(View.INVISIBLE);
+//            ImageView notsafe= findViewById(R.id.notsafe);
+//            notsafe.setVisibility(View.INVISIBLE);
+//            ImageView danger= findViewById(R.id.danger);
+//            danger.setVisibility(View.INVISIBLE);
             f.setChecked(false);
             b.setChecked(false);
             s.setChecked(false);
@@ -95,7 +95,7 @@ public class Sympcheck extends Activity {
 
         if (nC.isChecked()) {
             total = 1;
-            status(total);
+//            status(total);
             f.setChecked(false);
             b.setChecked(false);
             s.setChecked(false);
@@ -112,7 +112,7 @@ public class Sympcheck extends Activity {
             resetNone();
 
             total = total + 3;
-            status(total);
+//            status(total);
             System.out.println(total);
 
             symptoms[1] = "Fever";
@@ -125,7 +125,7 @@ public class Sympcheck extends Activity {
 
 
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println(total);
 
 
@@ -140,7 +140,7 @@ public class Sympcheck extends Activity {
 
 
             total = total + 8;
-            status(total);
+//            status(total);
             System.out.println(total);
 
 
@@ -155,7 +155,7 @@ public class Sympcheck extends Activity {
 
 
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println(total);
 
             symptoms[4] = "Shaking Chills";
@@ -168,7 +168,7 @@ public class Sympcheck extends Activity {
 
 
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println(total);
 
             symptoms[5] = "Diarrhea";
@@ -182,7 +182,7 @@ public class Sympcheck extends Activity {
 
 
             total = total + 2;
-            status(total);
+//            status(total);
             System.out.println(total);
 
             symptoms[0] = "Headache & Sore Throat";
@@ -194,56 +194,56 @@ public class Sympcheck extends Activity {
 
 
 
-        private void status(int tot){
-            ImageView ques = findViewById(R.id.question);
-            ImageView safe= findViewById(R.id.safe);
-            ImageView onverge= findViewById(R.id.onverge);
-            ImageView notsafe= findViewById(R.id.notsafe);
-            ImageView danger= findViewById(R.id.danger);
-            if(tot==1){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-
-            else if(tot>1 && tot < 4){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.VISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-            else if((tot >= 4) && (tot < 9)){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.VISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-            else if((tot >= 9) && (tot < 14)){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.VISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-            else if( tot >= 14 ){
-                ques.setVisibility(View.INVISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.VISIBLE);
-            } else {
-                //In case of zero or negative total
-                ques.setVisibility(View.VISIBLE);
-                safe.setVisibility(View.INVISIBLE);
-                onverge.setVisibility(View.INVISIBLE);
-                notsafe.setVisibility(View.INVISIBLE);
-                danger.setVisibility(View.INVISIBLE);
-            }
-        }
+//        private void status(int tot){
+//            ImageView ques = findViewById(R.id.question);
+//            ImageView safe= findViewById(R.id.safe);
+//            ImageView onverge= findViewById(R.id.onverge);
+//            ImageView notsafe= findViewById(R.id.notsafe);
+//            ImageView danger= findViewById(R.id.danger);
+//            if(tot==1){
+//                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//
+//            else if(tot>1 && tot < 4){
+//                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.VISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//            else if((tot >= 4) && (tot < 9)){
+//                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.VISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//            else if((tot >= 9) && (tot < 14)){
+//                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.VISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//            else if( tot >= 14 ){
+//                ques.setVisibility(View.INVISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.VISIBLE);
+//            } else {
+//                //In case of zero or negative total
+//                ques.setVisibility(View.VISIBLE);
+//                safe.setVisibility(View.INVISIBLE);
+//                onverge.setVisibility(View.INVISIBLE);
+//                notsafe.setVisibility(View.INVISIBLE);
+//                danger.setVisibility(View.INVISIBLE);
+//            }
+//        }
         private void resetSymptoms(){
             for(int i =0 ;i<6; ++i){
                 symptoms[i] = "";
